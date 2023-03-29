5d24ba62174d39bc8acbd66c41d457e6b9743b61
KiraCrypt <119428680+LuminosTruth@users.noreply.github.com>
2023-03-29 08:08:02 +0300

    Закончил адаптацию сайта под мобильные устройства

--- main/views.py
+++ main/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 
 def home(request):
-    return render(request, 'main/landing.html')
+    current_page = 'home'
+    return render(request, 'main/landing.html', {'current_page': current_page})
 
 
 def tech(request):
-    return render(request, 'main/tech.html')
+    current_page = 'tech'
+    return render(request, 'main/tech.html', {'current_page': current_page})
 
 
 def equipment(request):
-    return render(request, 'main/equipment.html')
+    current_page = 'equipment'
+    return render(request, 'main/equipment.html', {'current_page': current_page})
 
 
 def company(request):
-    return render(request, 'main/company.html')
+    current_page = 'company'
+    return render(request, 'main/company.html', {'current_page': current_page})
 
 
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    current_page = 'contacts'
+    return render(request, 'main/contacts.html', {'current_page': current_page})
